a99401ae96ca828d85ffeae81eb20d82c0764d31
Ninnin4901 <vinhduong04092001@gmail.com>
2022-11-14 10:28:42 +0700

    Add project files.

---
+++ Lap 1 - Back in 30 minutes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lap_1___Back_in_30_minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Khai báo biến chứa giá trị ở đây
+            string currentTime, minutesToAdd, 
+        }
+    }
+}
